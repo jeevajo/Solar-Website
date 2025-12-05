56ff74699b7f3cf09b026691ec6bc7d90be00ec4
jeeva <48005645+jeevajo@users.noreply.github.com>
2025-12-05 23:04:05 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+export default function App() {
+return (
+<div>
+<Navbar />
+<Hero />
+<Products />
+<Contact />
+<Footer />
+</div>
+);
+}
